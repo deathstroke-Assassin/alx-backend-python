79cc9422f4ed9db0b40e3373cce6a6044868c2f5
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:05:41 +0000

    v

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
-def add(a: float, b:float) -> float:
+''' python annotations '''
+def add(a: float, b: float) -> float:
+    ''' returns the sum of two floats as a float  '''
     return a + b
