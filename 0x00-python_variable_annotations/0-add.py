30974538c58b7e83d77cae31351784caa85fdb81
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:12:39 +0000

    vt

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''
 python annotations of t00.
+the add function.
 '''
 def add(a: float, b: float) -> float:
     '''
     returns the sum of two floats as a float.
+    task 0 solution.
     '''
     return a + b
