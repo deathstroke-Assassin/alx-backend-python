78dec7334735b8c747c44f82327c4fd655159dc8
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 01:28:38 +0000

    cz

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ python annotations of t00.
 from typing import List, Union, Tuple, Callable, Iterable, Sequence
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tulpe[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''
     annote the function
     '''
