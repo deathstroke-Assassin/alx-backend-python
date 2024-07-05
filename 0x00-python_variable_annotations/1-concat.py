091f2b01241447e45e0e52f1f653f5b01350b961
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:18:58 +0000

    v

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+python annotations of t00.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    returns the sum of two floats as a float.
+    task 0 solution.
+    '''
+    return str1 + str2
