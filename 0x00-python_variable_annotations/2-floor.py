e5c3e44b9f8cb002e5d7361a0a8a881a4196899d
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:23:23 +0000

    y

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+python annotations of t00.
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    returns the sum of two floats as a float.
+    task 0 solution.
+    '''
+    return int(n)
