ba2dde69dc8528e6ba6c02d6f05da70c6f53e126
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 01:13:10 +0000

    cfm

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+python annotations of t00.
+'''
+from typing import List, Union, Tuple, Collable
+
+
+def make_multiplier(multiplier: float) -> Collable[[float], float]:
+    '''
+    sum of inputs.
+    '''
+    return k: k * multiplier
