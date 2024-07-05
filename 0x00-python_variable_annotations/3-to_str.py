fcd2c8402f0b1553f3e9e87f7cb87c045b8676d1
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:25:45 +0000

    k

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+python annotations of t00.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    returns the sum of two floats as a float.
+    task 0 solution.
+    '''
+    return str(n)
