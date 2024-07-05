fb3047ebc809e73d5d46ca8b01ed454da4d67786
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:40:03 +0000

    i

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+python annotations of t00.
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''
+    sum of inputs 
+    '''
+    return float(sum(input_list))
