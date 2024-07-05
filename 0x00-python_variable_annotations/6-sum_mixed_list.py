d65362ad9fce4aaac3d4c7559b35d86e493cd875
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-05 00:56:35 +0000

    f

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 '''
 python annotations of t00.
 '''
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''
     sum of inputs.
     '''
