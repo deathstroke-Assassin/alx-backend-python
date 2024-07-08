3b73ea80e9af4bd49cc560567447ce724c8a4560
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-08 23:37:29 +0000

    vg

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import asyncio, random
+import asyncio
+import random
 
-async def wait_random(max_delay: int = 10) -> float
+
+async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
     '''
     wait_time = random.random() * max_delay
